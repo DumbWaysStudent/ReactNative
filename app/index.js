dd147dcd0c609e9774f41c790bacfc296c08b4bd
Ega Radiegtya <radiegtya@Egas-MacBook-Pro.local>
2017-08-10 16:23:34 +0700

    7. State

--- app/index.js
+++ app/index.js
@@ -3,8 +3,27 @@ import {View, Text, TouchableOpacity} from 'react-native';
 
 export default class Index extends React.Component {
 
+  constructor(){
+    super();
+    this.state = {
+      number: 0,
+      text: ""
+    };
+  }
+
+  componentDidMount(){
+    // setInterval(()=>{
+    //   this.setState({
+    //     number: this.state.number + 1
+    //   });
+    // }, 1000);
+  }
+
   handleClick(myName){
-    console.log('My name is ' + myName);
+    this.setState({
+      number: this.state.number + 1,
+      text: "Hello React Native"
+    })
   }
 
   render(){
@@ -13,6 +32,7 @@ export default class Index extends React.Component {
     return (
       <View>
         <Text>Please Click Button Below</Text>
+        <Text>{this.state.text}</Text>
         <TouchableOpacity onPress={()=> this.handleClick(myName)}>
           <Text>Click</Text>
         </TouchableOpacity>
