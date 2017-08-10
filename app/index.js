2f7c2dc6427ac46e56c15b5e3f58836b00f40d6f
Ega Radiegtya <radiegtya@Egas-MacBook-Pro.local>
2017-08-10 16:38:24 +0700

    8. Flexbox

--- app/index.js
+++ app/index.js
@@ -1,42 +1,39 @@
 import React from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
+import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
 
 export default class Index extends React.Component {
 
-  constructor(){
-    super();
-    this.state = {
-      number: 0,
-      text: ""
-    };
-  }
-
-  componentDidMount(){
-    // setInterval(()=>{
-    //   this.setState({
-    //     number: this.state.number + 1
-    //   });
-    // }, 1000);
-  }
-
-  handleClick(myName){
-    this.setState({
-      number: this.state.number + 1,
-      text: "Hello React Native"
-    })
-  }
-
   render(){
-    const myName = "Ega Wachid Radiegtya";
-
     return (
-      <View>
-        <Text>Please Click Button Below</Text>
-        <Text>{this.state.text}</Text>
-        <TouchableOpacity onPress={()=> this.handleClick(myName)}>
-          <Text>Click</Text>
-        </TouchableOpacity>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text>Header</Text>
+        </View>
+        <View style={styles.footer}>
+          <Text style={styles.footerText}>Footer</Text>
+        </View>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    flex: 0.2,
+    backgroundColor: "green"
+  },
+  footer: {
+    flex: 3,
+    backgroundColor: "blue",
+    justifyContent: "center",
+  },
+  footerText: {
+    alignSelf: 'center',
+    color: '#FFF',
+    fontWeight: 'bold',
+    fontSize: 20
+  }
+});
