6ba58a597f4b9148303d0de64a1f4d4513ec556f
Ega Radiegtya <radiegtya@Egas-MacBook-Pro.local>
2017-08-14 14:43:40 +0700

    13. React Native Navigation Android

--- index.android.js
+++ index.android.js
@@ -1,7 +1,23 @@
-import {
-  AppRegistry
-} from 'react-native';
+import { Navigation } from 'react-native-navigation';
 
-import Index from './app/index';
+import { registerScreens } from './app/screens';
 
-AppRegistry.registerComponent('LearnRn', () => Index);
+registerScreens(); // this is where you register all of your app's screens
+
+// start the app
+Navigation.startTabBasedApp({
+  tabs: [
+    {
+      label: 'One',
+      screen: 'example.FirstTabScreen', // this is a registered name for a screen
+      title: 'Screen One',
+      icon: require('./img/one.png'),
+    },
+    {
+      label: 'Two',
+      screen: 'example.SecondTabScreen',
+      title: 'Screen Two',
+      icon: require('./img/one.png'),
+    }
+  ]
+});
