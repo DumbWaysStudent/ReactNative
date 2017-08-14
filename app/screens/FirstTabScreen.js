183f851bcc17cabaa5a2b0faec0393dbb36af7a2
Ega Radiegtya <radiegtya@Egas-MacBook-Pro.local>
2017-08-14 13:27:34 +0700

    12. React Native Navigation iOS

---
+++ app/screens/FirstTabScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Container, Content, Text} from 'native-base';
+
+export default FirstTabScreen = () => (
+  <Container>
+    <Content>
+      <Text>FirstTabScreen</Text>
+    </Content>
+  </Container>
+)
