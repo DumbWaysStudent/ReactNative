6aea71f9bbc6d0a13f4148c318cd361c4e3bd856
Ega Radiegtya <radiegtya@Egas-MacBook-Pro.local>
2017-08-15 13:43:46 +0700

    18. MLH Project - Home dan Settings Page

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,10 +1,41 @@
 import React from 'react';
-import {Container, Content, Text} from 'native-base';
+import {Container, Content, Text, Thumbnail, Button} from 'native-base';
 
 export default Home = () => (
-  <Container>
+  <Container style={{justifyContent: "center", alignSelf: "center"}}>
     <Content>
-      <Text>Home</Text>
+      <Thumbnail
+        large
+        source={{uri: 'http://tool.mobilelegendshack.n-qz.com/logo.png'}}
+        style={{alignItems: 'center', width: 250, height: 200}}
+      />
+
+      <Text style={nbStyles.subtitle}>
+        Welcome to Mobile Legends
+      </Text>
+      <Text style={nbStyles.subtitle}>
+        Heroes Dictionary
+      </Text>
+      <Text style={nbStyles.subtitle}>
+        Start Exploring/Creating
+      </Text>
+      <Text style={nbStyles.subtitle}>
+        Your Favourites Heroes
+      </Text>
+
+      <Button block style={nbStyles.btn}>
+        <Text>Start</Text>
+      </Button>
     </Content>
   </Container>
 )
+
+const nbStyles = {
+  subtitle: {
+    textAlign: 'center',
+    color: '#ACD2FA'
+  },
+  btn: {
+    marginTop: 15
+  }
+}
